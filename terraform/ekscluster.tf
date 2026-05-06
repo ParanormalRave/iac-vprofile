f0ba2cd7843b41fe07314b71b5c6836862e5130f
rave <mercyadenubi2@gmail.com>
2026-05-06 01:08:01 +0100

    Correction of the terraform file and the addition of ECR repo

--- terraform/ekscluster.tf
+++ terraform/ekscluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.19.1"
+  version = "~> 20.31"
 
   cluster_name    = local.cluster_name
   cluster_version = "1.27"
@@ -10,9 +10,10 @@ module "eks" {
 
   cluster_endpoint_public_access = true
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-  }
+ eks_managed_node_group_defaults = {
+  ami_type = "AL2023_x86_64_STANDARD"
+}
+
 
   eks_managed_node_groups = {
     one = {
